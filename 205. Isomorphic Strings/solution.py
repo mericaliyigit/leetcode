00ba00c40e2b205fff53c01e09e27c49ca797c11
Meric Ali Yigit <mericaliyigit@gmail.com>
2023-06-08 13:11:58 +0100

    isomorphic

---
+++ 205. Isomorphic Strings/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t = {}
+        t_to_s = {}
+        for i in range(len(s)):
+            if s[i] in s_to_t:
+                if s_to_t[s[i]] != t[i]:
+                    return False
+            else:
+                s_to_t[s[i]] = t[i]
+
+            if t[i] in t_to_s:
+                if t_to_s[t[i]] != s[i]:
+                    return False
+            else:
+                t_to_s[t[i]] = s[i]
+        return True
+
+
+print(Solution().isIsomorphic(s="egg", t="add"))
+print(Solution().isIsomorphic(s="foo", t="bar"))
+print(Solution().isIsomorphic(s="paper", t="title"))
+print(Solution().isIsomorphic(s="badc", t="baba"))
